41ae493d30cce0649bb94064def0a7b7bb4caf65
Paul Eagle <pauleagle@gmail.com>
2013-08-22 18:34:34 -0700

    Create New File
    asdf

---
+++ Prray.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+* prray class has array data conversion and collections capabilities
+* @author microuser
+* @copyright MIT-2
+* @version 0.01a
+*
+* <pre>
+* Useage:
+* $p = new Prray
+* $p->add(array(1,2,3)
+* $p->add(array(0=>'a'
+*                ,1=>'b'
+*                ,2=>'c'
+*                );
+* $p->add(array(0=>'a',1=>array('1.1));
+
+* </pre>
+*/
+class Prray {
+
+/*
+*the array holder
+*/
+public $collection;
+
+__construct(){
+
+}//end function __construct
+
+}//end class Prray
+
+?>
